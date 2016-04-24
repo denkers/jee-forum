0d63ca4bf8d579e81128b69397a3c44fbdc3fd6d
denkers <kyle.business@inbox.com>
2016-04-25 01:44:36 +1200

    Added database/HibernateUtil

---
+++ jforum-war/src/java/com/kyleruss/jforum/database/HibernateUtil.java
@@ -0,0 +1,37 @@
+//=========================================
+//  KYLE RUSSELL
+//  AUT UNIVERSITY 2016
+//  https://github.com/denkers/jforum
+//=========================================
+
+package com.kyleruss.jforum.database;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY;
+    
+    static 
+    {
+        try 
+        {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            SESSION_FACTORY = new AnnotationConfiguration().configure().buildSessionFactory();
+        } 
+        
+        catch (Throwable ex)
+        {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory()
+    {
+        return SESSION_FACTORY;
+    }
+}
